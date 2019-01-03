1b5f8afcbd2731140e269d9e258b00008a5b1e16
BernardG7500 <45844379+BennieBen@users.noreply.github.com>
2019-01-03 16:40:23 +0100

    met commentaar

--- index.php
+++ index.php
@@ -2,17 +2,21 @@
 
 $getallen = array();
 
+// 10 random getallen
 for ($x=0; $x < 10; $x++) {
 	$getallen[$x] = rand(1, 100);
 	echo "$x $getallen[$x] <br>";
 }
 
+// bepaal het hoogste getal
 $max = 0;
 for ($x=0; $x < 10; $x++) {
 	if ($getallen[$x] > $max) {
 		$max = $getallen[$x];
 	}
 }
+
+// het resultaat
 echo "<br>Het grootste getal is: " . $max . "<br>";
 
 ?>
